2224f850fbc76ac48e111470b085b7b2cc6bf59c
Darman <dahlem.romain@gmail.com>
2019-05-29 12:31:34 +0200

    Cours de la monnaie sur la liste

--- src/main/java/org/crptoMoney/exchange/GettingRessource.java
+++ src/main/java/org/crptoMoney/exchange/GettingRessource.java
@@ -1,9 +1,12 @@
 package org.crptoMoney.exchange;
 
 import org.crptoMoney.exchange.model.CryptoMoney;
+import org.crptoMoney.exchange.model.CryptoMoneyCours;
+import org.crptoMoney.exchange.model.CryptoMoneyUser;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -19,8 +22,44 @@ public class GettingRessource {
     }*/
 
     @GET
+    @Path("/cryptoMoney")
     @Produces(MediaType.APPLICATION_JSON)
     public List<CryptoMoney> getAllCryptoMoney() {
         return CryptoMoney.listAll();
+    }
+
+    @GET
+    @Path("/cryptoMoney/sigle/{sigle}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<CryptoMoney> getCryptoMoneyBySigle(@PathParam("sigle") String sigle) {
+        return CryptoMoney.list("miniIdMonnaieCrypto",sigle);
+    }
+
+    @GET
+    @Path("/cryptoMoney/name/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<CryptoMoney> getCryptoMoneyByName(@PathParam("name") String name) {
+        return CryptoMoney.list("nomMonnaieCrypto",name);
+    }
+
+    @GET
+    @Path("/cryptoMoney/price/{price}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<CryptoMoney> getCryptoMoneyByPrice(@PathParam("price") String price) {
+        return CryptoMoney.list("price",price);
+    }
+
+    @GET
+    @Path("/cryptoMoney/cours/{sigle}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<CryptoMoneyCours> getCryptoMoneyCoursBySigle(@PathParam("sigle") String sigle) {
+        return CryptoMoneyCours.list("sigle",sigle);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/cryptoMoneyUser")
+    public List<CryptoMoneyUser> getAllCryptoMoneyUser() {
+        return CryptoMoneyUser.listAll();
     }
 }
--- src/main/java/org/crptoMoney/exchange/model/CryptoMoneyCours.java
+++ src/main/java/org/crptoMoney/exchange/model/CryptoMoneyCours.java
@@ -1,11 +1,13 @@
 package org.crptoMoney.exchange.model;
 
+
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.persistence.Entity;
 
 @Entity
-public class CryptoMoney extends PanacheEntity {
-    public String miniIdMonnaieCrypto;
-    public String nomMonnaieCrypto;
+public class CryptoMoneyCours extends PanacheEntity {
+    public String sigle;
+    public String price;
+    public String dateMoney;
 }
